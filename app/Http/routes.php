78ae76ddeef294cd30961a6b7296328d84c8e8f1
Yaron Tal <yaron.tal@oneshoe.nl>
2016-01-27 18:33:05 +0100

    Authentication

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -14,4 +14,14 @@
 Route::group(['middleware' => ['web']], function () {
     Route::get('/', 'BeerController@Frontpage');
     Route::get('/json', 'BeerController@Json');
+
+    Route::get('/youtube', [
+      'middleware' => 'auth',
+      'uses' => 'YoutubeController@index'
+    ]);
+
+    // Authentication routes...
+    Route::get('auth/login', 'Auth\AuthController@getLogin');
+    Route::post('auth/login', 'Auth\AuthController@postLogin');
+    Route::get('auth/logout', 'Auth\AuthController@getLogout');
 });
